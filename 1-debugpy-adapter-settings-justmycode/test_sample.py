921244f5dafe37f4870ca1f7d951126e28dd5517
blakeNaccarato <blake.naccarato@gmail.com>
2024-07-24 09:25:25 -0700

    Reduce duplication

--- 1-debugpy-adapter-settings-justmycode/test_sample.py
+++ 1-debugpy-adapter-settings-justmycode/test_sample.py
@@ -2,4 +2,3 @@ import json
 
 def test_answer():
     assert json.dumps({'toto': 'tata'}) == "{'toto': 'tata'}"
-    assert json.dumps({'toto': 'tata'}) == "{'toto': 'tata'}"
--- 3-vscode-python-debug-adapter/sample.py
+++ 3-vscode-python-debug-adapter/sample.py
@@ -1,4 +1,3 @@
 import json
 
 json.dumps({'toto': 'tata'}) == "{'toto': 'tata'}"
-json.dumps({'toto': 'tata'}) == "{'toto': 'tata'}"
